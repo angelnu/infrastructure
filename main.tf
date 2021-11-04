b9081051e5694aacec1965419f61eb7f2e5f89d0
angelnu <git@angelnucom>
2021-11-04 00:14:58 +0000

    save variables in SOPS secret

--- main.tf
+++ main.tf
@@ -1,17 +1,30 @@
 
+terraform {
+  required_providers {
+    sops = {
+      source = "carlpett/sops"
+      version = "0.6.3"
+    }
+  }
+}
+
 locals {
   network_clients = csvdecode(file("${path.module}/settings/network_clients.csv"))
+}
+
+data "sops_file" "settings_secrets" {
+  source_file = "${path.module}/settings/secrets.yaml"
 }
 
 module "unifi" {
   source = "./unifi"
 
   network_clients     = local.network_clients
-  unifi_api_url       = var.unifi_api_url
-  unifi_api_insecure  = var.unifi_api_insecure
-  unifi_api_username  = var.unifi_api_username
-  unifi_api_password  = var.unifi_api_password
-  unifi_wlan_password = var.unifi_wlan_password
+  unifi_api_url       = data.sops_file.settings_secrets.data["unifi.api.url"]
+  unifi_api_insecure  = data.sops_file.settings_secrets.data["unifi.api.insecure"]
+  unifi_api_username  = data.sops_file.settings_secrets.data["unifi.api.username"]
+  unifi_api_password  = data.sops_file.settings_secrets.data["unifi.api.password"]
+  unifi_wlan_password = data.sops_file.settings_secrets.data["unifi.wlan.password"]
 }
 
 module "openwrt" {

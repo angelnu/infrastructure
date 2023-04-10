839cef4b410c29e3b0fef04a74012b36d138460c
angelnu <git@angelnu.com>
2023-04-10 16:44:40 +0000

    new login app

--- terraform/authentik/app-recipes.tf
+++ terraform/authentik/app-recipes.tf
@@ -4,12 +4,12 @@ resource "authentik_provider_oauth2" "recipes" {
   client_secret      = var.authentik_config.apps.recipes.client_secret
   authorization_flow = authentik_flow.authorization_implicit_consent.uuid
   token_validity    = "days=30"
-  redirect_uris     = ["http://recipes.pub.${var.main_home_domain}/accounts/tandoor/login/callback/"]
+  redirect_uris     = ["https://recipes.pub.${var.main_home_domain}/accounts/authentik/login/callback/"]
 }
 
 resource "authentik_application" "recipes" {
-  name              = "recipes app"
-  slug              = "recipes-app"
+  name              = "recipes"
+  slug              = "recipes"
   protocol_provider = authentik_provider_oauth2.recipes.id
   meta_launch_url   = "https://recipes.pub.${var.main_home_domain}"
 }

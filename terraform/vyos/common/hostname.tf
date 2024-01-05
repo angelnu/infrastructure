b3d4286d136518689979d06c2305c7ad24a2837a
angelnu <git@angelnu.com>
2024-01-05 17:32:51 +0000

    set hostname

---
+++ terraform/vyos/common/hostname.tf
@@ -0,0 +1,10 @@
+resource "vyos_config_block_tree" "hostname" {
+  path = "system host-name"
+
+  configs = {
+    ""      = var.config.hostname,
+  }
+  depends_on = [
+    vyos_config_block_tree.eth0
+  ]
+}

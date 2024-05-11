ac6ae7f938caffa492c2d70be147c5f0212ca50c
Angel Nunez Mencias <git@angelnu.com>
2024-05-11 12:01:18 +0000

    add scopes to gitea oauth

--- terraform/authentik/app-gitea.tf
+++ terraform/authentik/app-gitea.tf
@@ -2,7 +2,10 @@ resource "authentik_provider_oauth2" "gitea" {
   name                   = "gitea"
   client_id              = "gitea"
   client_secret          = var.authentik_config.apps.gitea.client_secret
+  authentication_flow    = authentik_flow.login.uuid
   authorization_flow     = authentik_flow.authorization_implicit_consent.uuid
+  property_mappings      = data.authentik_scope_mapping.oauth2.ids
+  signing_key            = data.authentik_certificate_key_pair.generated.id
   access_token_validity  = "hours=1"
   refresh_token_validity = "days=30"
   redirect_uris          = ["https://git.${var.main_home_domain}/user/oauth2/authentik/callback"]
